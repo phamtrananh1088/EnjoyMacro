d954b83845522f4b4cdf4e28efe7b1a6f0e3a7a8
phamtrananh1088 <phamtrananh1088@gmail.com>
2024-11-25 15:18:11 +0700

    ver1.0.0

--- Sources/EnjoyMacro/EnjoyMacro.swift
+++ Sources/EnjoyMacro/EnjoyMacro.swift
@@ -9,3 +9,22 @@
 /// produces a tuple `(x + y, "x + y")`.
 @freestanding(expression)
 public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "EnjoyMacroMacros", type: "StringifyMacro")
+
+@available(macOS 12,*)
+@freestanding(expression)
+public macro dateBuilded() -> String = #externalMacro(module: "EnjoyMacroMacros", type: "DateBuildedMacro")
+
+@attached(member, names: named(recordChange))
+public macro recordChange() = #externalMacro(module: "EnjoyMacroMacros", type: "RecordChangeMacro")
+
+@attached(member, names: named(sync))
+public macro sync() = #externalMacro(module: "EnjoyMacroMacros", type: "SyncMacro")
+
+@attached(member, names: named(tableName))
+public macro tableName(_ argument: String) = #externalMacro(module: "EnjoyMacroMacros", type: "TableNameMacro")
+
+@attached(member, names: named(createTable))
+public macro createTable(_ tableName: String, _ primaryKey: String?) = #externalMacro(module: "EnjoyMacroMacros", type: "CreateTableMacro")
+
+@attached(peer, names: arbitrary)
+public macro combineFuture() = #externalMacro(module: "EnjoyMacroMacros", type: "CombineFutureMacro")
--- Sources/EnjoyMacroMacros/EnjoyMacroMacro.swift
+++ Sources/EnjoyMacroMacros/EnjoyMacroMacro.swift
@@ -1,4 +1,5 @@
 import SwiftCompilerPlugin
+import Foundation
 import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
@@ -24,10 +25,147 @@ public struct StringifyMacro: ExpressionMacro {
         return "(\(argument), \(literal: argument.description))"
     }
 }
+@available(iOS 15, *)
+@available(macOS 12, *)
+public struct DateBuildedMacro: ExpressionMacro {
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) -> ExprSyntax {
+        let date = ISO8601DateFormatter().string(from: .now)
 
+        return "\"\(raw: date)\""
+    }
+}
+
+public struct RecordChangeMacro: MemberMacro {
+    public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        return [DeclSyntax("mutating func recordChange() {"),
+                DeclSyntax("    sync = SyncStatus.changed.rawValue"),
+                DeclSyntax("}")]
+    }
+}
+
+public struct SyncMacro:  MemberMacro {
+    public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        return [DeclSyntax("var sync: Int = SyncStatus.pending.rawValue")]
+    }
+}
+
+public struct TableNameMacro:  MemberMacro {
+    public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        guard case .argumentList(let args) = node.arguments,
+        let argument = args.first?.expression else {
+            fatalError("Missing argument for parameter of tableNameMacro")
+        }
+        return [DeclSyntax("static let tableName: String = \(argument)")]
+    }
+}
+
+public struct CreateTableMacro: MemberMacro {
+    
+    public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        guard case .argumentList(let args) = node.arguments,
+        let argument = args.first?.expression.as(StringLiteralExprSyntax.self) else {
+            fatalError("Missing argument for parameter of createTableMacro")
+        }
+        guard let structDecl = declaration.as(StructDeclSyntax.self) else {
+            fatalError("createTableMacro: invalidSyntax")
+        }
+        var properties: [(name: String, type: String)] = []
+        for member in structDecl.memberBlock.members {
+            if let varDecl = member.decl.as(VariableDeclSyntax.self) {
+                for binding in varDecl.bindings {
+                    if let identifier = binding.pattern.as(IdentifierPatternSyntax.self),
+                       let type = binding.typeAnnotation?.type.description {
+                        properties.append((name: identifier.identifier.text, type: map(type)))
+                    }
+                }
+            }
+        }
+
+        var ds = [DeclSyntax("static func createTable() -> String {"),
+                  DeclSyntax("  return \"\"\""),
+                  DeclSyntax("  CREATE TABLE IF NOT EXISTS \(raw: argument.representedLiteralValue!)(")
+        ]
+        for property in properties {
+            ds.append(DeclSyntax("  \(raw: property.name) \(raw: property.type),"))
+        }
+        if case .argumentList(let args) = node.arguments,
+           args.count > 1 {
+            var arguments: [StringLiteralExprSyntax?] = []
+            for argument in args {
+                arguments.append(argument.expression.as(StringLiteralExprSyntax.self))
+            }
+            ds.append(DeclSyntax("  PRIMARY KEY (\(raw: arguments[1]!.representedLiteralValue!))"))
+        }
+        ds.append(DeclSyntax("  );"))
+        ds.append(DeclSyntax("  \"\"\""))
+        ds.append(DeclSyntax("}"))
+        return ds
+    }
+    static func map(_ argument: String) -> String {
+        switch argument {
+        case "String":
+            return "TEXT"
+        case "Int", "Bool":
+            return "INTERGER"
+        case "Double", "Float":
+            return "REAL"
+        case "Date":
+            return "TEXT"
+        case "Data":
+            return "BLOB"
+        default:
+            return "TEXT"
+        }
+    }
+}
+
+public struct CombineFutureMacro: PeerMacro {
+    public static func expansion(of node: AttributeSyntax, providingPeersOf declaration: some DeclSyntaxProtocol, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
+        guard let f = declaration.as(FunctionDeclSyntax.self) else {
+            fatalError("this macro apply only on func")
+        }
+        var fu: [String] = [
+        ]
+        let throwsSpec = f.signature.effectSpecifiers?.throwsSpecifier?.text
+
+        for p in f.signature.parameterClause.parameters {
+            fu.append("\(p.firstName): \(p.firstName)")
+        }
+        return [
+"""
+func \(f.name)Future\(f.signature.parameterClause)-> Future<\(raw: f.signature.returnClause?.type.description.trimmingCharacters(in: CharacterSet(charactersIn: " ")) ?? "Void"), Error> {
+    return Future { promise in
+        Task {\(raw: throwsSpec == nil ? "" : """
+            try {
+""")
+            let data = \(raw: throwsSpec == nil ? " " : "try ")self.\(f.name)(\(raw: fu.joined(separator: ",")))
+            promise(.success(data))\(raw: throwsSpec == nil ? "" : """
+            } catch {
+                promise(.failure(error))
+            }
+""")
+        }
+    }
+}
+"""
+        ]
+    }
+}
+
+@available(iOS 15, *)
+@available(macOS 12, *)
 @main
 struct EnjoyMacroPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
         StringifyMacro.self,
+        DateBuildedMacro.self,
+        RecordChangeMacro.self,
+        SyncMacro.self,
+        TableNameMacro.self,
+        CreateTableMacro.self,
+        CombineFutureMacro.self
     ]
 }
